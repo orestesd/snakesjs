377fd29a371e8e10c8f5de84bbe841613d789075
Orestes Domínguez <tes.orestes@gmail.com>
2012-06-29 09:05:57 +0200

    the snake can turn left or right

--- snakes.js
+++ snakes.js
@@ -1,9 +1,20 @@
 
+var DIRECTIONS = {UP:0, RIGHT:1, BOTTOM:2, LEFT:3}
+
 /*** PLAYER ***/
 
 var Player = function(id, name) {
 	var _id = id;
 	var _name = name;
+	var _direction = DIRECTIONS.UP;
+	
+	this.turn = function(dir) {
+		if (DIRECTIONS.LEFT === dir) {
+			_direction = (_direction - 1 + 4) % 4;
+		} else if (DIRECTIONS.RIGHT === dir) {
+			_direction = (_direction + 1) % 4;
+		}
+	}
 	
 	this.getId = function() {
 		return _id;
@@ -12,8 +23,13 @@ var Player = function(id, name) {
 	this.getName = function() {
 		return _name;
 	}
+	
+	this.getDirection = function() {
+		return _direction;
+	}
 }
 
 function createPlayer(id, name) {return new Player(id, name)};
 
 exports.createPlayer = createPlayer;
+exports.DIRECTIONS = DIRECTIONS;
--- spec/game.spec.js
+++ spec/game.spec.js
@@ -10,4 +10,37 @@ describe("player", function() {
 		expect(player.getId()).toEqual(1);
 		expect(player.getName()).toEqual('john');
 	});
+	
+	it("the snake changes direction with four keys (default direction = UP)", function() {
+		var player = snakes.createPlayer(1, 'john');
+		
+		expect(player.getDirection()).toEqual(snakes.DIRECTIONS.UP);
+		
+		// turn left
+		player.turn(snakes.DIRECTIONS.LEFT);
+		expect(player.getDirection()).toEqual(snakes.DIRECTIONS.LEFT);
+		
+		player.turn(snakes.DIRECTIONS.LEFT);
+		expect(player.getDirection()).toEqual(snakes.DIRECTIONS.BOTTOM);
+		
+		player.turn(snakes.DIRECTIONS.LEFT);
+		expect(player.getDirection()).toEqual(snakes.DIRECTIONS.RIGHT);
+		
+		player.turn(snakes.DIRECTIONS.LEFT);
+		expect(player.getDirection()).toEqual(snakes.DIRECTIONS.UP);
+		
+		// turn right
+		player.turn(snakes.DIRECTIONS.RIGHT);
+		expect(player.getDirection()).toEqual(snakes.DIRECTIONS.RIGHT);
+		
+		player.turn(snakes.DIRECTIONS.RIGHT);
+		expect(player.getDirection()).toEqual(snakes.DIRECTIONS.BOTTOM);
+		
+		player.turn(snakes.DIRECTIONS.RIGHT);
+		expect(player.getDirection()).toEqual(snakes.DIRECTIONS.LEFT);
+		
+		player.turn(snakes.DIRECTIONS.RIGHT);
+		expect(player.getDirection()).toEqual(snakes.DIRECTIONS.UP);
+		
+	});
 });
