4f951d7ee0cee73d3a523619399b338bce6969e0
Orestes Domínguez <tes.orestes@gmail.com>
2012-07-01 10:43:49 +0200

    test code refactor to include beforeEach

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -2,19 +2,18 @@ var snakes = require('../snakes.js');
 var topologies = require('../topologies.js');
 
 describe("player", function() {
-
+	var player;
+	
 	beforeEach(function() {
+		player = snakes.createPlayer(1, 'john');
 	});
 
 	it("you can create a player with an id and a name", function() {
-		var player = snakes.createPlayer(1, 'john');
 		expect(player.getId()).toEqual(1);
 		expect(player.getName()).toEqual('john');
 	});
 	
 	it("the snake can turn left or right  (default direction = UP)", function() {
-		var player = snakes.createPlayer(1, 'john');
-		
 		expect(player.getDirection()).toEqual(snakes.DIRECTIONS.UP);
 		
 		// turn left
@@ -46,15 +45,11 @@ describe("player", function() {
 	});
 	
 	it("the snake has a head position", function() {
-		var player = snakes.createPlayer(1, 'john');
-
 		player.moveTo([1, 2]);
 		expect(player.getHeadPosition()).toEqual([1, 2]);
 	});
 
 	it("the snake can move step by step and the body follow the head", function() {
-		var player = snakes.createPlayer(1, 'john');
-		
 		expect(player.getTailSize()).toEqual(5);
 		expect(player.getPositions().length).toEqual(0);
 		
@@ -76,8 +71,6 @@ describe("player", function() {
 	});	
 
 	it("the snake's tail size can grow, shrink and reset", function() {
-		var player = snakes.createPlayer(1, 'john');
-
 		expect(player.getTailSize()).toEqual(5);
 		player.grow(3);
 		expect(player.getTailSize()).toEqual(8);
@@ -109,19 +102,19 @@ describe("player", function() {
 });
 
 describe("world", function() {
-	
+	var topo, world;
+		
+	beforeEach(function() {
+		topo = topologies.getAll().walled;
+		world = snakes.createWorld(topo);
+	});
+		
 	it("world size is calculated ok from a topology", function() {
-		var topo = topologies.get('walled');
-		var world = snakes.createWorld(topo);
-
 		expect(world.getSize().h).toEqual(topo.grid.length);
 		expect(world.getSize().w).toEqual(topo.grid[0].length);
 	});
 	
 	it("world can return the type of the position x,y", function() {
-		var topo = topologies.getAll().walled;
-		var world = snakes.createWorld(topo);
-
 		expect(world.getTile([0,1])).toEqual(snakes.TILE_TYPES.WALL);
 		expect(world.getTile([3,11])).toEqual(snakes.TILE_TYPES.WALL);
 		expect(world.getTile([1,2])).toEqual(snakes.TILE_TYPES.EMPTY);
