72e611c73c056e5906ebcdbb982c6e697b0d31cf
Orestes Domínguez <tes.orestes@gmail.com>
2012-07-29 14:01:49 +0200

    server offers socket.io client

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 
 var connect = require('connect'),
-	http = require('http'),
-	GameIO = new require('./gameio.js');
+	http = require('http');
+	
 
 var myserver = {host:'127.0.0.1', port:8090};
 
 var app_connect = connect().use(connect.static(__dirname+'/client'));
 
 var app = http.createServer(app_connect).listen(myserver.port);
+
+var io = require('socket.io').listen(app),
+	GameIO = new require('./gameio.js').init(io);
 
 console.log('server started: ', myserver);
