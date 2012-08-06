975214a7cefe468717629238c7866d36e9bd9f12
Orestes Domínguez <tes.orestes@gmail.com>
2012-08-06 17:35:38 +0200

    player can join game in ui

--- gameio.js
+++ gameio.js
@@ -7,8 +7,8 @@ var gamejs = require('./game.js'),
 
 var games = {};
 
-var update_clients_freq = 1000 / 5;
-var update_game_freq = 1000 / 5;
+var update_clients_freq = 1000 / 7;
+var update_game_freq = 1000 / 7;
 
 function init_io(io){
 
@@ -44,13 +44,14 @@ function init_io(io){
 
     socket.on('join-game', function(game_id){
       var joined_game = getGame(game_id);
+
       if (joined_game && ! joined_game.isStarted()) {
         game = joined_game;
         player = snakes.createPlayer(socket.id, client_name);
         game.addPlayer(player);
 
         socket.join(game.id);
-        io.sockets.in(game.id).emit('game-joined', {game_id:game.id, player_names: game.getPlayerNames()}); // for himself too
+        io.sockets.in(game.id).emit('game-joined', {game_id:game.id, topology:game.getWorld().getTopology(), player_names: game.getPlayerNames()}); // for himself too
         // socket.broadcast.to(game.id).emit('game-joined', {game_id:game.id}); // not for himself
 
         console.log('[%s] joined game %s', socket.id, game.id);
@@ -76,6 +77,7 @@ function init_io(io){
 
     socket.on('command', function(command){ 
       if (game && player) {
+        console.log('command');
         player.turn(command.dir);
       }
     });
@@ -100,7 +102,6 @@ function getGame(id) {
 } 
 
 function initUpdateClientsInterval(io, game) {
-  var freq = 1000 / 5;
   var interval = setInterval(function() {
       var game_status = game.getStatus();
 
@@ -109,14 +110,13 @@ function initUpdateClientsInterval(io, game) {
 
       io.sockets.in(game.id).emit('game-status', game_status);
   
-  }, freq);
+  }, update_clients_freq);
 
   return interval;
 }
 
 function initUpdateGameInterval(io, game) {
   var interval = setInterval(function() {
-
       game.step();
 
   }, update_game_freq);
